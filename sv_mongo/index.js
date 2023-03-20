973be27ef3a184b226a4e2a993340686c5b41961
huytgio <huytgio147@gmail.com>
2023-03-20 12:02:16 +0700

    1.0.4

--- sv_mongo/index.js
+++ sv_mongo/index.js
@@ -4,12 +4,13 @@ const mongoose = require('mongoose')
 const authRouter = require('./routers/auth')
 const postRouter = require('./routers/post')
 const cmtRouter = require('./routers/comment')
-const connectDB = async() => {
+// Connect to the database and set wide ACLs. This is called once per request
+const connectDB = async () => {
     try {
         await mongoose.connect(`mongodb+srv://huytgio147:147852Huytgio@letmeknow.vdxja2f.mongodb.net/LetmeKnow?retryWrites=true&w=majority`,
-        {
-            
-        })
+            {
+
+            })
         console.log('mongoDB connected')
     } catch (error) {
         console.log(error.message)
@@ -18,14 +19,15 @@ const connectDB = async() => {
 }
 connectDB()
 
+// Create and return a service application that will listen on port 5000. This is a bit tricky because we don't want to listen on port 5000 in a multi - process environment
 const app = express()
 app.use(express.json())
 
-app.use('/api/auth',authRouter)
-app.use('/api/posts',postRouter)
-app.use('/api/comment',cmtRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
+app.use('/api/comment', cmtRouter)
 
-app.get('/',(req,res) => res.send('cmmdmm'))
+app.get('/', (req, res) => res.send('cmmdmm'))
 
 const PORT = 5000
-app.listen(PORT, ()=> console.log(`sv started on port ${PORT}`))
+app.listen(PORT, () => console.log(`sv started on port ${PORT}`))
