6909378134cc30f28a7d80d796bbdb548631cd7e
huytgio <huytgio147@gmail.com>
2023-03-23 23:59:12 +0700

    null

--- client/src/components/posts/ActionButton.js
+++ client/src/components/posts/ActionButton.js
@@ -8,7 +8,7 @@ import React, { useContext } from 'react'
 import { PostContext } from "../../contexts/PostContext"
 import Container from "react-bootstrap/esm/Container"
 import Row from "react-bootstrap/esm/Row"
-
+import { Link } from "react-router-dom"
 
 const ActionButton = ({ url, _id }) => {
     const { deletePost, findPost, setShowUpdatePostModal, postState: { post } } = useContext(PostContext)
@@ -21,13 +21,14 @@ const ActionButton = ({ url, _id }) => {
     return (
         <Container fluid>
 
-            <Row>
-                <Button variant="outline-info" href={url} target='_blank' size="sm" >
-                    <img src={playIcon} alt='play' width='32' height='32' />
-                    <br></br>
-                    Xem
-                </Button>
-            </Row>
+            <Link to='/detail'>
+                <Row>
+                    <Button variant="outline-info" onClick={choosePost.bind(this, _id)} size="sm" >
+                        <img src={playIcon} alt='play' width='32' height='32' />
+                        Xem
+                    </Button>
+                </Row>
+            </Link>
             <Row>
                 <Button variant="outline-success" onClick={choosePost.bind(this, _id)} size="sm">
                     <img src={editIcon} alt='play' width='32' height='32' />
